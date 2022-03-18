adc6ea83e1e71500e8827e0008f1faf83575581c
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-03-18 14:21:27 +0000

    basically working

--- scripts.js
+++ scripts.js
@@ -21,49 +21,45 @@ updateBtn.addEventListener("click", getMetalPrice);
 // https://www.goldapi.io/dashboard
 
 const API_KEY1 = "goldapi-15j9sm18l0w7rfer-io";
-const gold1 = "XAU";
+const metal = "XAU";
+const currency = "GBP";
 const updateBtn1 = document.querySelector(".btn-update");
 
 var myHeaders = new Headers();
 myHeaders.append("x-access-token", "goldapi-15j9sm18l0w7rfer-io");
 myHeaders.append("Content-Type", "application/json");
-/*let timestam;
-let date = new Date(timestam * 1000);
-const datevalues = [
-  date.getFullYear(),
-  date.getMonth() + 1,
-  date.getDate(),
-  date.getHours(),
-  date.getMinutes(),
-  date.getSeconds(),
-].join(":");
-*/
+
+const getTimestamp = timestamp => {
+  let date = new Date(timestamp * 1000);
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  let hours = date.getHours();
+  let mins = date.getMinutes();
+  let secs = date.getSeconds();
+
+  let callDate = `${year}-${month.toString().padStart(2, 0)}-${day
+    .toString()
+    .padStart(2, 0)} ${hours.toString().padStart(2, 0)}:${mins
+    .toString()
+    .padStart(2, 0)}:${secs.toString().padStart(2, 0)}`;
+
+  return callDate;
+};
 var requestOptions = {
   method: "GET",
   headers: myHeaders,
   redirect: "follow",
 };
 const getMetalPrice1 = () => {
-  fetch("https://www.goldapi.io/api/XAU/GBP", requestOptions)
+  fetch(`https://www.goldapi.io/api/${metal}/${currency}`, requestOptions)
     .then(response => response.json())
     .then(result => {
       console.log(result);
-      let date = new Date(result.timestamp * 1000);
-
-      //const datevalues = [
-      let year = date.getFullYear();
-      let month = date.getMonth() + 1;
-      let day = date.getDate();
-      let hours = date.getHours();
-      let mins = date.getMinutes();
-      let secs = date.getSeconds();
+      let dateTime = getTimestamp(result.timestamp);
       console.log(`
-      ${year} ${month.toString().padStart(2, 0)} ${day
-        .toString()
-        .padStart(2, 0)} ${hours.toString().padStart(2, 0)} ${mins
-        .toString()
-        .padStart(2, 0)} ${secs.toString().padStart(2, 0)}
-      Gold:-
+      ${dateTime}
+      ${result.metal}:-
       Day low: ${result.low_price}
       Day high: ${result.high_price}
       Price: ${result.price}`);
