56a9a72dfc570d02a0a18ae928bdc9ea96c04a41
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-03-24 13:14:59 +0000

    config update

--- src/js/config.js
+++ src/js/config.js
@@ -1,8 +1,5 @@
 export const API_KEY = 'goldapi-15j9sm18l0w7rfer-io';
 export const API_URL = 'https://www.goldapi.io/api';
 
-
-
 //console.log(`https://www.goldapi.io/api/${metal}/${currency}/${historicDate}`);
 //endpoint - https://www.goldapi.io/api/:symbol/:currency/:date?
-
