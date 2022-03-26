60693cf5a0b5f507dcafdeeb32c133b7124c0a00
evariste1963 <evariste@hotmail.co.uk>
2022-03-26 19:08:17 +0000

    view

--- src/js/views/view.js
+++ src/js/views/view.js
@@ -1,1 +1,16 @@
-export default class View {}
+import icons from 'url:../../img/icons.svg';
+/*
+export default class View {
+  renderSpinner() {
+    const markup = `
+                <div class="spinner">
+                  <svg>
+                    <use href="${icons}#icon-loader"></use>
+                  </svg>
+                </div>
+        `;
+    this._clear(); //clear out HTML container
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+}
+*/
