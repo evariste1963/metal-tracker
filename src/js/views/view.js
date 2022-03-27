5f1f38cf154a57d0abf1687c6b96c5ce63f4107a
evariste1963 <evariste@hotmail.co.uk>
2022-03-27 21:33:57 +0100

    spinner

--- src/js/views/view.js
+++ src/js/views/view.js
@@ -1,1 +1,16 @@
 export default class View {}
+/*
+export default class View {
+  renderSpinner() {
+    const markup = `
+                <div class="spinner">
+                  <svg>
+                    <use href="${icons}#icon-loader"></use>
+                  </svg>
+                </div>
+        `;
+    this._clear(); //clear out HTML container
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+}
+*/
