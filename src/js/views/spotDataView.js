2fa766c05d649988968f0ee04e23c080889596c7
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-03-29 15:53:04 +0100

    day percenatage change

--- src/js/views/spotDataView.js
+++ src/js/views/spotDataView.js
@@ -5,13 +5,16 @@ class SpotDataView extends View {
     super();
   }
 
-  _generateMarkup = function (result, callTime) {
+  _generateMarkup(result, callTime) {
     return !result.date
       ? `
     <H1 ><span>Latest Price</span><br>
     <div id=callTime>${callTime}</div>
     <div id= spotBox>
-    <span id=spotPrice >£ ${result.price}</span>
+    <p id=spotPrice >£ ${result.price}  <span id=change>${(
+          ((result.price - result.prev_close_price) / result.prev_close_price) *
+          100
+        ).toFixed(2)}%</span></p>
     <div id=buy><span>BUY:  £${result.ask}</span></div>
     <div id=sell><span>SELL:  £${result.bid}</span></div>
     </div>
@@ -20,6 +23,6 @@ class SpotDataView extends View {
   ${callTime}
   Previous closing price: ${result.prev_close_price}
   Price: ${result.price}`);
-  };
+  }
 }
 export default new SpotDataView();
