6fe9731f93a38e7ad2d9d0929f6b2a6505299ee1
evariste1963 <evariste@hotmail.co.uk>
2022-04-11 17:19:40 +0100

    slight adjustment to accstatus

--- src/js/views/accUpdateView.js
+++ src/js/views/accUpdateView.js
@@ -1,26 +1,22 @@
 import View from './view';
 
 class AccountUpdateView extends View {
-  _parentElement = document.querySelector('accStatus');
+  _parentElement = document.querySelector('#accStatus');
   _errorMessage = 'No account data found for your query! Please try again ;)';
   _message = '';
 
   _generateAccMarkup(result) {
     return `
        <div id='accBox'>
-        <H1 >account status</span><br>
-        <div id='accDetails'
           <p> Requests today: <span>${result.requests_today}</span></p>
           <p> Requests remaining this month: <span>${
             500 - result.requests_month
           }</span></p>
           <p> Requests last month: <span>${
             result.requests_last_month
-          }</span></p></p>
-        </div>
-      </div>
-      </H1>
-      `;
+          }</span></p>
+          </div>
+        `;
   }
 }
 
