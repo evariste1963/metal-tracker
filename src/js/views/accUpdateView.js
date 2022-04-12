386402905a699a4d829d20251fd9c5a157331e73
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-04-12 12:14:53 +0100

    button hover colors

--- src/js/views/accUpdateView.js
+++ src/js/views/accUpdateView.js
@@ -3,7 +3,7 @@ import View from './view';
 
 class AccountUpdateView extends View {
   _parentElement = document.querySelector('#accStatus');
-  _errorMessage = 'No account data found for your query! Please try again ;)';
+  _errorMessage = 'Account data could not be found! Please try again later';
   _message = 'updating account status, please wait...';
 
   _generateAccMarkup(result) {
