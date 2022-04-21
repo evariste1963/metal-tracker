fe9836cdc070a0b23fb40c4ffde33c3ddc6934a1
evariste1963 <evariste@hotmail.co.uk>
2022-04-21 20:34:17 +0100

    test4

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -53,11 +53,12 @@ export let devAccount = {
 /////////////////////////////////////////////////////////// DELETE DEVdATA BLOCK//////////////
 
 export const AJAX = async function (url, requestOptions) {
-  this.url = url; //THIS IS WHERE NETLIFY PROBLEM IS!!!!!!! CANNOT ASIGN THIS.URL
-  //this.url = `https://www.goldapi.io/api/XAU/GBP`;
-  this.requestOptions = requestOptions;
   try {
-    const fetchPro = fetch(url, this.requestOptions);
+    this.url = url; //THIS IS WHERE NETLIFY PROBLEM IS!!!!!!! CANNOT ASIGN THIS.URL
+    //this.url = `https://www.goldapi.io/api/XAU/GBP`;
+    this.requestOptions = requestOptions;
+
+    const fetchPro = fetch(this.chpurl, this.requestOptions);
     const response = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
     const result = await response.json();
     if (!response.ok) throw new Error(`ooops, something went wrong!!`);
