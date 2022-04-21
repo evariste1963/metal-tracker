ead21a70e85e14bd1f86869d95b7b882482ecb01
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-04-21 16:02:19 +0100

    back to pre-trying -- working locally

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -54,8 +54,8 @@ export let devAccount = {
 
 export const AJAX = async function (url, requestOptions) {
   try {
-    //this.url = url; //THIS IS WHERE NETLIFY PROBLEM IS!!!!!!! CANNOT ASIGN THIS.URL
-    this.url = `https://www.goldapi.io/api/XAU/GBP`;
+    this.url = url; //THIS IS WHERE NETLIFY PROBLEM IS!!!!!!! CANNOT ASIGN THIS.URL
+    //this.url = `https://www.goldapi.io/api/XAU/GBP`;
     this.requestOptions = requestOptions;
     const fetchPro = fetch(this.url, this.requestOptions);
     const response = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
